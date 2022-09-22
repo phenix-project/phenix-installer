fed6c51cbd106d99d928110fc5bc1729dcea3e0e
Billy K. Poon <bkpoon@lbl.gov>
2022-09-22 09:02:38 -0700

    Add more post-installation steps
    [skip ci]

--- constructor/scripts/post_install.sh
+++ constructor/scripts/post_install.sh
@@ -1,4 +1,15 @@
+# compile Python files
+echo ""
+echo "Compiling Python files"
+echo "======================"
+pushd ${PREFIX}/lib
+${PREFIX}/bin/libtbx.py_compile_all -i
+popd
+
 # write phenix_env files
+echo ""
+echo "Writing phenix_env files"
+echo "========================"
 SCRIPT_LOCATION=`${PREFIX}/bin/libtbx.find_in_repositories libtbx/auto_build/conda_build/write_env_files.py`
 
 ${PREFIX}/bin/python ${SCRIPT_LOCATION} \
@@ -9,5 +20,10 @@ ${PREFIX}/bin/python ${SCRIPT_LOCATION} \
   --destination ${PREFIX}
 
 # rebuild rotarama and cablam caches
+echo ""
+echo "Rebuilding rotarama and cablam caches"
+echo "====================================="
 ${PREFIX}/bin/mmtbx.rebuild_rotarama_cache
 ${PREFIX}/bin/mmtbx.rebuild_cablam_cache
+
+echo ""
