803735f041aba373095a150c491ef05a095ad769
Billy K. Poon <bkpoon@lbl.gov>
2023-06-07 06:54:20 -0700

    Consolidate linux image clean up

---
+++ scripts/clean_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+rm -fr ${ANDROID_HOME}
+
+rm -fr ${JAVA_HOME_8_X64}
+rm -fr ${JAVA_HOME_11_X64}
+rm -fr ${JAVA_HOME_17_X64}
+
+rm -fr ${CHROMEWEBDRIVER}
+rm -fr ${EDGEWEBDRIVER}
+rm -fr ${GECKOWEBDRIVER}
