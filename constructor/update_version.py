51304b8a2467c0647eba5f10e389f6e101535736
Billy K. Poon <bkpoon@lbl.gov>
2023-06-09 10:52:32 -0700

    Fix version update

--- constructor/update_version.py
+++ constructor/update_version.py
@@ -6,19 +6,31 @@ import datetime
 import os
 import sys
 
+def get_version_from_file(filename):
+  version = os.path.basename(filename).split('-')[1]  # dev.1234 or 1.23.4567
+  split_version = version.split('.')
+  build_number = split_version[-1]  # 1234
+  if len(split_version) > 2:
+    version = '.'.join(split_version[:2])  # 1.23
+  else:
+    version = split_version[0]  # dev
+  version = '-'.join([version, build_number])  # dev-1234 or 1.23-4567
+  return version
+
 def update_version(filename, pkg_name, version):
   # check if version is a filename (e.g. conda package with version)
   # version follows the Phenix format (e.g. dev-1234 or 1.23-4567)
   # conda package naming does not allow hyphens (e.g. dev.1234 or 1.23.4567)
   if os.path.isfile(version):
-    version = os.path.basename(version).split('-')[1]  # dev.1234 or 1.23.4567
-    split_version = version.split('.')
-    build_number = split_version[-1]  # 1234
-    if len(split_version) > 2:
-      version = '.'.join(split_version[:2])  # 1.23
-    else:
-      version = split_version[0]  # dev
-    version = '-'.join([version, build_number])  # dev-1234 or 1.23-4567
+    version = get_version_from_file(version)
+  elif version == 'windows':  # special handling for Windows
+    search_dir = 'D:\\a\\1\\win-64'
+    contents = os.listdir(search_dir)
+    for f in contents:
+      f = os.path.join(search_dir, f)
+      if os.path.isfile(f) and (f.endswith('conda') or f.endswith('bz2')):
+        version = get_version_from_file(f)
+        break
   assert os.path.isfile(filename)
   with open(filename, 'r') as f:
     lines = f.readlines()
