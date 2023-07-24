c0570bb63e6df1f564ea3c7e16d81093c585e1d7
Billy K. Poon <bkpoon@lbl.gov>
2023-07-24 13:44:15 -0700

    Clean up

--- scripts/clean_linux.sh
+++ scripts/clean_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -xe
+# set -xe
 
 sudo mkdir -p /opt/empty_dir || true
 for d in \
@@ -13,12 +13,16 @@ for d in \
         /usr/share/dotnet \
         /usr/share/swift \
         ; do
+  echo $d
   time sudo rsync --stats -a --delete /opt/empty_dir/ $d || true
+  echo
 done
 time sudo apt-get purge -y -f firefox \
                           google-chrome-stable \
                           microsoft-edge-stable
 time sudo apt-get autoremove -y >& /dev/null
 time sudo apt-get autoclean -y >& /dev/null
+echo
 time sudo docker image prune --all --force
+echo
 df -h
